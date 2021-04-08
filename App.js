ee3ed699049d63e03f84127c78da710e26198839
Aliel Reyes <alielreyes@gmail.com>
2021-04-07 21:40:58 -0700

    Added a welcome page

--- App.js
+++ App.js
@@ -1,62 +1,12 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Platform, StatusBar, View, Dimensions } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   
-  // console.log(Dimensions.get("screen"));
-  const {landscape} = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "purple",
-          flex: 1,
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          flexWrap: 'wrap',
-          alignContent: 'center'
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "dodgerblue",
-            width: 100,
-            height: 100,
-          }}
-        ></View>
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 100,
-            height: 100,
-          }}
-        ></View>
-        <View
-          style={{
-            backgroundColor: "tomato",
-            width: 100,
-            height: 100,
-          }}
-        ></View>
-        <View
-          style={{
-            backgroundColor: "gray",
-            width: 100,
-            height: 100,
-          }}
-        ></View>
-        <View
-          style={{
-            backgroundColor: "lime",
-            width: 100,
-            height: 100,
-          }}
-        ></View>
-      </View>
-    </SafeAreaView>
+    <WelcomeScreen />
   );
 }
 
